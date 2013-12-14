2c98f5493cf3966b09e9ffe0af02f5935c7dbb7d
Pieter Joost van de Sande <pj@born2code.net>
2013-12-14 13:56:08 +0100

    Accept tunnel connections

--- server/main.go
+++ server/main.go
@@ -4,33 +4,67 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"net/http"
 )
 
 var (
-	address = flag.String("address", "0.0.0.0:8080", "The address to host on")
+	hostname = flag.String("hostname", "publichost.me", "The domain to host on")
 )
 
 type Tunnel struct {
-	Name string
+	host       *TunnelServiceHost
+	connection net.Conn
 }
 
-type Packet struct {
+func NewTunnel(host *TunnelServiceHost, connection net.Conn) *Tunnel {
+	return &Tunnel{
+		host:       host,
+		connection: connection,
+	}
+}
+
+func (t *Tunnel) HandleHttp(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusTeapot)
+}
+
+func (t *Tunnel) Serve() {
+	buffer := make([]byte, 4096)
+	defer t.connection.Close()
+
+	for {
+		n, err := t.connection.Read(buffer)
+		if err != nil {
+			fmt.Printf("Error reading from %v: %v", t.connection.RemoteAddr(), err)
+			break
+		}
+
+		msg := buffer[0:n]
+		fmt.Printf("Recieved data from %v: %v", t.connection.RemoteAddr(), string(msg))
+	}
+
+	fmt.Printf("Closing tunnel from %v")
+}
+
+type TunnelServiceHost struct {
 }
 
 func main() {
-	flag.Parse()
-	accepted := make(chan *net.TCPConn, 50)
+	host := &TunnelServiceHost{}
+	if err := host.ListenAndServePH(); err != nil {
+		fmt.Sprintf("[FATAL] Error serving PH: %v", err)
+	}
+}
 
-	addr, err := net.ResolveTCPAddr("tcp", *address)
+func (t *TunnelServiceHost) ListenAndServePH() error {
+	address := "0.0.0.0:8080"
+	resolved, err := net.ResolveTCPAddr("tcp", address)
 	if err != nil {
-		fmt.Printf("Cannot resolve address %v: %v\n", *address, err)
-		return
+		return fmt.Errorf("Cannot resolve address %v: %v\n", address, err)
 	}
 
-	listener, err := net.ListenTCP("tcp", addr)
+	listener, err := net.ListenTCP("tcp", resolved)
 	if err != nil {
-		fmt.Printf("Cannot start listening on address %v: %v\n", *address, err)
-		return
+		return fmt.Errorf("Cannot start listening on address %v: %v\n", resolved, err)
 	}
 
 	defer listener.Close()
@@ -45,6 +79,23 @@ func main() {
 
 		fmt.Printf("Accepted connection from %v to %v:%v\n", connection.RemoteAddr(), connection.LocalAddr().Network(), connection.LocalAddr().String())
 
-		accepted <- connection
+		tunnel := NewTunnel(t, connection)
+		go tunnel.Serve()
 	}
 }
+
+// func (t *TunnelServiceHost) ListenAndServeHTTP() error {
+// 	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+// 		hostname := request.URL.Host
+
+// 		t.rw.Lock()
+// 		defer t.rw.Unlock()
+
+// 		if tunnel, ok := t.tunnels[hostname]; ok {
+// 			tunnel.HandleHttp(response, request)
+// 		} else {
+// 			response.WriteHeader(http.StatusNotFound)
+// 		}
+// 	})
+// 	return http.ListenAndServe("", nil)
+// }
