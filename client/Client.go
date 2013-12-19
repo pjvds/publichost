502bfb829c78e0f0af9cb00fbed84b6f56ccf2a5
Pieter Joost van de Sande <pj@born2code.net>
2013-12-19 21:47:58 +0100

    Finalize very basic client and server implementation

--- client/Client.go
+++ client/Client.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"net/http"
@@ -11,6 +12,8 @@ type ClientConnection interface {
 
 type clientConnection struct {
 	connection net.Conn
+	reader     *bufio.Reader
+	writer     *bufio.Writer
 }
 
 func NewClientConnection(hostname string) (ClientConnection, error) {
@@ -27,11 +30,30 @@ func NewClientConnection(hostname string) (ClientConnection, error) {
 	return handshake(conn)
 }
 
+// Will handshake with the server. The caller is responsible
+// for closing the connection on an error.
 func handshake(conn net.Conn) (ClientConnection, error) {
-	request, _ := http.NewRequest("CONNECT", "tunneler.publichost.me", nil)
-	request.Header.Set("hostname", "firstclient.publichost.me")
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
 
-	request.Write(conn)
+	request, _ := http.NewRequest("CONNECT", "tunneler.publichost.me", nil)
+	request.Header.Set("Connection", "Keep-Alive")
+	request.Header.Set("X-PublicHost-HostName", "firstclient.publichost.me")
+
+	if err := request.Write(writer); err != nil {
+		return nil, err
+	}
+	if err := writer.Flush(); err != nil {
+		return nil, err
+	}
+
+	response, err := http.ReadResponse(reader, request)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Cannot create tunnel: %v (%v)", response.Status, response.StatusCode)
+	}
 
 	// TODO: Negociate protocol version, etc.
 	return clientConnection{
--- server/Server.go
+++ server/Server.go
@@ -1,10 +1,51 @@
 package server
 
 import (
+	"bufio"
+	"errors"
+	"net"
 	"net/http"
+	"sync"
 )
 
-type TunnelServer struct {
+var (
+	ErrAlreadyExists = errors.New("tunnel already exists")
+)
+
+type TunnelFrontEnd struct {
+	Hostname string
+	lock     sync.Mutex
+
+	connection net.Conn
+	readWriter *bufio.ReadWriter
+}
+
+func (t *TunnelFrontEnd) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	if err := request.Write(t.readWriter); err != nil {
+		log.Error("Unable to write request to tunnel: %v", err)
+	}
+
+	conn, rw, err := response.(http.Hijacker).Hijack()
+	if err != nil {
+		log.Error("Error hijacking tunnel response: %v", err)
+		response.WriteHeader(http.StatusInternalServerError)
+	}
+	defer conn.Close()
+
+	r, err := http.ReadResponse(rw.Reader, request)
+	if err != nil {
+		log.Error("Unable to read response from tunnel: %v", err)
+	}
+
+	if err := r.Write(rw.Writer); err != nil {
+		log.Error("Unable to write response from front end: %v", err)
+	}
+	if err := rw.Writer.Flush(); err != nil {
+		log.Error("Unable to write response from front end: %v", err)
+	}
 }
 
 type Server interface {
@@ -12,7 +53,9 @@ type Server interface {
 }
 
 type server struct {
-	address string // The TCP address to listen on,
+	address string // The TCP address to listen on
+	lock    sync.Mutex
+	tunnels map[string]*TunnelFrontEnd
 }
 
 func (s *server) ListenAndServe() error {
@@ -20,16 +63,77 @@ func (s *server) ListenAndServe() error {
 }
 
 func (s *server) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	if request.URL.Host != "tunneler.publichost.me" {
-		response.WriteHeader(http.StatusNotFound)
+	if request.URL.Host == "tunneler.publichost.me" {
+		s.handleNewTunnelRequest(response, request)
 		return
+	} else {
+		s.handleHttpTraffic(response, request)
 	}
+}
 
-	var hostname string
-	if hostname = request.Header.Get("hostname"); hostname == "" {
+func (s *server) handleNewTunnelRequest(response http.ResponseWriter, request *http.Request) {
+	if request.Method != "CONNECT" {
 		response.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	response.Header().Set("hostname", hostname)
+	var hostname string
+	if hostname = request.Header.Get("X-PublicHost-HostName"); hostname == "" {
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	connection, readWriter, err := response.(http.Hijacker).Hijack()
+	if err != nil {
+		log.Error("Unable to hijack response: %v", err)
+		response.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	tunnel, err := s.addNewTunnelByHostname(hostname, connection, readWriter)
+	if err == ErrAlreadyExists {
+		response.WriteHeader(http.StatusNotModified)
+	}
+
+	response.Header().Set("X-PublicHost-HostName", tunnel.Hostname)
+}
+
+func (s *server) handleHttpTraffic(response http.ResponseWriter, request *http.Request) {
+	tunnel := s.getTunnelByHostname(request.URL.Host)
+	if tunnel == nil {
+		response.WriteHeader(http.StatusBadGateway)
+		return
+	}
+
+	tunnel.ServeHTTP(response, request)
+}
+
+func (s *server) addNewTunnelByHostname(hostname string, connection net.Conn, readWriter *bufio.ReadWriter) (*TunnelFrontEnd, error) {
+	tunnel := &TunnelFrontEnd{
+		Hostname:   hostname,
+		connection: connection,
+		readWriter: readWriter,
+	}
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if _, ok := s.tunnels[hostname]; ok {
+		return nil, ErrAlreadyExists
+	}
+
+	s.tunnels[hostname] = tunnel
+	return tunnel, nil
+}
+
+// Returns the matching tunnel, or nil if not found.
+func (s *server) getTunnelByHostname(hostname string) *TunnelFrontEnd {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if tunnel, ok := s.tunnels[hostname]; ok {
+		return tunnel
+	}
+
+	return nil
 }
