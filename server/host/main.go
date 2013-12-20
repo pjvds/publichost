f6bd9b366acd5533470dad70f1a40b9832e30485
Pieter Joost van de Sande <pj@born2code.net>
2013-12-20 08:18:37 +0100

    Adds address flag to host

--- server/host/main.go
+++ server/host/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"flag"
 	"github.com/pjvds/publichost/server"
 )
 
+var (
+	address = flag.String("address", "0.0.0.0:80", "the address to listen on")
+)
+
 func main() {
-	address := ":http"
+	flag.Parse()
+	log.Info("Starting publichost server at %v", *address)
 
-	log.Info("Starting publichost server at %v", address)
-
-	server := server.NewServer(address)
+	server := server.NewServer(*address)
 	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Error serving at %v: %v", address, err)
+		log.Fatalf("Error serving at %v: %v", *address, err)
 	}
 }
