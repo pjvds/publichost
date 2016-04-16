78cb90edb20f3634af86933dd2ef831ad23d0b8c
Pieter Joost van de Sande <pj@born2code.net>
2016-04-16 20:49:58 +0200

    refactor server

--- server/main.go
+++ server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import(
+    "strconv"
+    "strings"
     "net"
     "net/http"
     "github.com/codegangsta/cli"
@@ -11,6 +13,82 @@ import(
     "io"
     "sync"
 )
+
+type TunnelSession struct {
+    id int
+    session *yamux.Session
+    remoteAddress string
+}
+
+func (this TunnelSession) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+    conn, err := this.session.Open()
+    if err != nil {
+        log.Println(err.Error())
+        response.Write([]byte("<html><body>" + err.Error() + "</body></html>"))
+        return
+    }
+    defer conn.Close()
+
+    if err := request.Write(conn); err != nil {
+        log.Println(err.Error())
+        return
+    }
+
+    tunnelResponse, err := http.ReadResponse(bufio.NewReader(conn), request)
+    if err != nil {
+        response.Write([]byte("<html><body>" + err.Error() + "</body></html>"))
+        return
+    }
+
+    for header, values := range tunnelResponse.Header {
+        for _, value := range values {
+            response.Header().Add(header, value)
+        }
+    }
+
+    response.WriteHeader(tunnelResponse.StatusCode)
+    if tunnelResponse.Body != nil {
+        io.Copy(response, tunnelResponse.Body)
+    }
+}
+
+// Accepts new tunnel session in a blocking fashion. When it returns it closes the listener
+// and returns with the error why it returned.
+func Accept(accepted chan TunnelSession, publicHostname string, listener net.Listener) error {
+    defer listener.Close()
+
+    for id := 0; ; id++{
+         conn, err := listener.Accept()
+         if err != nil {
+             return err
+         }
+         log.Printf("connection accepted %v\n", conn.RemoteAddr())
+
+         // perform handshake
+         go func(conn net.Conn, id int) {
+            hostname := fmt.Sprintf("%v.%v", id, publicHostname)
+            publicAddress := fmt.Sprintf("http://%v", hostname)
+
+            if _, err := conn.Write([]byte(publicAddress + "\n")); err != nil {
+                log.Println(err.Error())
+                return
+            }
+
+            session, err := yamux.Client(conn, nil)
+            if err != nil {
+                log.Println(err.Error())
+                return
+            }
+
+            log.Printf("tunnel created %v->%v\n", publicAddress, conn.RemoteAddr())
+            accepted <- TunnelSession{
+                id,
+                session,
+                publicAddress,
+            }
+         }(conn, id)
+    }
+}
 
 func main() {
     app := cli.NewApp()
@@ -37,81 +115,44 @@ func main() {
             log.Fatal(err.Error())
         }
 
-        tunnels := make(map[string]*yamux.Session)
+
+        accepted := make(chan TunnelSession)
+        go Accept(accepted, "localhost:8080", listener)
+
         var tunnelsLock sync.RWMutex
+        tunnels := make(map[int]TunnelSession)
 
         go http.ListenAndServe(httpAddress, http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+            if !strings.HasSuffix(request.Host, ".localhost:8080") {
+                log.Println("invalid hostname")
+                response.Write([]byte("<html><body>invalid hostname</body></html>"))
+                return
+            }
+
+            id, err := strconv.Atoi(strings.TrimSuffix(request.Host, ".localhost:8080"))
+            if err != nil {
+                log.Println("invalid tunnel id: " + err.Error())
+                response.Write([]byte("<html><body>invalid tunnel id: "+err.Error()+"</body></html>"))
+                return
+            }
+
             tunnelsLock.RLock()
-
-            id := request.Host
-            session, ok := tunnels[id]
+            tunnel, ok := tunnels[id]
             tunnelsLock.RUnlock()
-            
+
             if !ok {
-                response.Write([]byte("<html><body>no session found with id <strong>" + id + "</strong></body></html>"))
-                tunnelsLock.RUnlock()
+                log.Printf("no tunnel with id %v", id)
+                response.Write([]byte("<html><body>no session found with id <strong>" + strconv.Itoa(id) + "</strong></body></html>"))
                 return
             }
 
-            conn, err := session.Open()
-            if err != nil {
-                log.Println(err.Error())
-                response.Write([]byte("<html><body>" + err.Error() + "</body></html>"))
-                return
-            }
-
-            if err := request.Write(conn); err != nil {
-                log.Println(err.Error())
-                return
-            }
-
-            tunnelResponse, err := http.ReadResponse(bufio.NewReader(conn), request)
-            if err != nil {
-                response.Write([]byte("<html><body>" + err.Error() + "</body></html>"))
-                return
-            }
-
-            for header, values := range tunnelResponse.Header {
-                for _, value := range values {
-                    response.Header().Add(header, value)
-                }
-            }
-
-            response.WriteHeader(tunnelResponse.StatusCode)
-            if tunnelResponse.Body != nil {
-                io.Copy(response, tunnelResponse.Body)
-            }
+            tunnel.ServeHTTP(response, request)
         }))
 
-        id := 0
-        for {
-            id++
-            conn, err := listener.Accept()
-            if err != nil {
-                log.Fatal(err.Error())
-            }
-
-            go func(conn net.Conn, id int) {
-                hostname := fmt.Sprintf("%v.%v", id, httpAddress)
-                remoteAddress := fmt.Sprintf("http://%v", hostname)
-
-                if _, err := conn.Write([]byte(remoteAddress + "\n")); err != nil {
-                    log.Println(err.Error())
-                    return
-                }
-
-                    session, err :=yamux.Client(conn, nil)
-                    if err != nil {
-                        log.Println(err.Error())
-                        return
-                    }
-
-                    tunnelsLock.Lock()
-                    tunnels[hostname] = session
-                    tunnelsLock.Unlock()
-
-                    log.Println(hostname + " tunnel active")
-            }(conn, id)
+        for session := range accepted {
+            tunnelsLock.Lock()
+            tunnels[session.id] = session
+            tunnelsLock.Unlock()
         }
     }
     app.RunAndExitOnError()
