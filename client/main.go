93bd4317254a937c0982a4b3d1f5b2133c900903
pjvds <pj@born2code.net>
2016-11-17 21:58:06 -0800

    remove -url flag

--- client/main.go
+++ client/main.go
@@ -9,9 +9,9 @@ import (
 	"net/url"
 	"os"
 
-	"github.com/codegangsta/cli"
 	"github.com/gorilla/handlers"
 	"github.com/hashicorp/yamux"
+	"github.com/urfave/cli"
 )
 
 func main() {
@@ -19,53 +19,85 @@ func main() {
 	app.Name = "publichost"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:   "url",
-			Value:  "http://localhost:3000",
-			Usage:  "the local url to make publicly available",
-			EnvVar: "URL",
-		},
-		cli.StringFlag{
 			Name:   "publichost, p",
 			Value:  "",
 			Usage:  "the address of the publichost server",
 			EnvVar: "PUBLICHOST",
 		},
 	}
-	app.Action = func(ctx *cli.Context) {
-		localUrl := ctx.String("url")
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name: "dir",
+			Action: func(ctx *cli.Context) {
+				localDir := ctx.Args().First()
 
-		log.Println("connecting to server")
-		conn, err := tls.Dial("tcp", "api.publichost.io:443", nil)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		if _, err := conn.Write([]byte("GET /tunnel HTTP/1.1\r\nHost: api.publichost.io\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n")); err != nil {
-			log.Fatal(err.Error())
-		}
+				log.Println("connecting to server")
+				conn, err := tls.Dial("tcp", "api.publichost.io:443", nil)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				if _, err = conn.Write([]byte("GET /tunnel HTTP/1.1\r\nHost: api.publichost.io\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n")); err != nil {
+					log.Fatal(err.Error())
+				}
 
-		log.Println("opening tunnel")
-		reader := bufio.NewReader(conn)
-		response, err := http.ReadResponse(reader, nil)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+				log.Println("opening tunnel")
+				reader := bufio.NewReader(conn)
+				response, err := http.ReadResponse(reader, nil)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
 
-		log.Print("tunnel available at: " + response.Header.Get("X-Publichost-Address"))
-		tunnel, err := yamux.Server(conn, nil)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+				log.Print("tunnel available at: " + response.Header.Get("X-Publichost-Address"))
+				tunnel, err := yamux.Server(conn, nil)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
 
-		local, err := url.Parse(localUrl)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+				handler := handlers.CombinedLoggingHandler(os.Stdout, http.FileServer(http.Dir(localDir)))
+				if err := http.Serve(tunnel, handler); err != nil {
+					log.Fatal(err.Error())
+				}
+			},
+		},
+		cli.Command{
+			Name: "http",
+			Action: func(ctx *cli.Context) {
+				localUrl := ctx.Args().First()
 
-		handler := handlers.CombinedLoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(local))
+				log.Println("connecting to server")
+				conn, err := tls.Dial("tcp", "api.publichost.io:443", nil)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				if _, err = conn.Write([]byte("GET /tunnel HTTP/1.1\r\nHost: api.publichost.io\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n")); err != nil {
+					log.Fatal(err.Error())
+				}
 
-		if err := http.Serve(tunnel, handler); err != nil {
-			log.Fatal(err.Error())
-		}
+				log.Println("opening tunnel")
+				reader := bufio.NewReader(conn)
+				response, err := http.ReadResponse(reader, nil)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+
+				log.Print("tunnel available at: " + response.Header.Get("X-Publichost-Address"))
+				tunnel, err := yamux.Server(conn, nil)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+
+				local, err := url.Parse(localUrl)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+
+				handler := handlers.CombinedLoggingHandler(os.Stdout, httputil.NewSingleHostReverseProxy(local))
+
+				if err := http.Serve(tunnel, handler); err != nil {
+					log.Fatal(err.Error())
+				}
+			},
+		},
 	}
 	app.RunAndExitOnError()
 }
